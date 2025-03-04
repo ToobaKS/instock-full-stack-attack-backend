bbfb9d25bb04481c1ebbe4b283a18930be804f65
Tooba Sheikh <tksheikh17@gmail.com>
2025-03-04 14:31:50 -0500

    added an index file to use express and listen to port

---
+++ index.js
@@ -0,0 +1,11 @@
+import "dotenv/config";
+import express from "express";
+const app = express();
+
+const PORT = process.env.PORT || 5050;
+
+app.use(express.json());
+
+app.listen(PORT, () => {
+  console.log(`running at http://localhost:${PORT}`);
+});
