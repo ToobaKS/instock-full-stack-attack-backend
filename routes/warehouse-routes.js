219073f31149e9d458366aa519253e55c21fe310
NicoleBiii <xinyuebi23@gmail.com>
2025-03-05 10:55:53 -0500

    add api to delete a warehouse

--- routes/warehouse-routes.js
+++ routes/warehouse-routes.js
@@ -4,6 +4,9 @@ import * as warehouseController from "../controllers/warehouse-controller.js";
 const router = express.Router();
 
 router.route("/").get(warehouseController.getAll);
-router.route("/:id").get(warehouseController.findOne)
+
+router.route("/:id")
+.get(warehouseController.findOne)
+.delete(warehouseController.deleteWarehouse);
 
 export default router;
