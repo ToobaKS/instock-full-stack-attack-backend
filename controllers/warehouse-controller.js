6971277775c39359face49c1635b48baa5ff2a2a
NicoleBiii <xinyuebi23@gmail.com>
2025-03-05 10:58:41 -0500

    Revert "add api to delete a warehouse"
    This reverts commit 219073f31149e9d458366aa519253e55c21fe310.

--- controllers/warehouse-controller.js
+++ controllers/warehouse-controller.js
@@ -12,43 +12,23 @@ const getAll = async (_req, res) => {
     }
   };
 
-const findOne = async (req, res) => {
-  try {
-    const warehouseFound = await knex("warehouses").where({ id: req.params.id });
-
-    if (warehouseFound.length === 0) {
-      return res.status(404).json({
-        message: `Warehouse with ID ${req.params.id} not found`,
+  const findOne = async (req, res) => {
+    try {
+      const warehouseFound = await knex("warehouses").where({ id: req.params.id });
+  
+      if (warehouseFound.length === 0) {
+        return res.status(404).json({
+          message: `Warehouse with ID ${req.params.id} not found`,
+        });
+      }
+  
+      const warehouseData = warehouseFound[0];
+      res.json(warehouseData);
+    } catch (error) {
+      res.status(500).json({
+        message: `Unable to retrieve warehouse data for warehouse with ID ${req.params.id}`,
       });
     }
+  };
 
-    const warehouseData = warehouseFound[0];
-    res.json(warehouseData);
-  } catch (error) {
-    res.status(500).json({
-      message: `Unable to retrieve warehouse data for warehouse with ID ${req.params.id}`,
-    });
-  }
-};
-
-const deleteWarehouse = async(req, res) => {
-  const warehouseId = req.params.id;
-  try {
-    const warehouseFound = await knex("warehouses").where({ id: warehouseId });
-    if (warehouseFound.length === 0) {
-      return res.status(404).json({
-        message:`Warehouse with ID ${warehouseId} not found`,
-      });
-    }
-    await knex("inventories").where({ warehouse_id: warehouseId }).del();
-    await knex("warehouses").where({ id:warehouseId }).del();
-    res.status(204).send();
-  } catch (error) {
-    res.status(500).json({
-      message: `Unable to delete warehouse with ID ${warehouseId}: ${error}`,
-    });
-  }
-}
-
-
-export { getAll, findOne, deleteWarehouse };
+  export { getAll, findOne };
--- routes/warehouse-routes.js
+++ routes/warehouse-routes.js
@@ -4,9 +4,6 @@ import * as warehouseController from "../controllers/warehouse-controller.js";
 const router = express.Router();
 
 router.route("/").get(warehouseController.getAll);
-
-router.route("/:id")
-.get(warehouseController.findOne)
-.delete(warehouseController.deleteWarehouse);
+router.route("/:id").get(warehouseController.findOne)
 
 export default router;
