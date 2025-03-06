816deebe8b3004eab4aa283ed2a830309b3782e6
samihasamin <samihasamin@hotmail.com>
2025-03-06 01:29:25 -0500

    Add PUT route for editing warehouse details and mount routes in index.js

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 import "dotenv/config";
 import express from "express";
 const app = express();
+import warehouseRoutes from "./routes/warehouseRoutes.js";
 
 const PORT = process.env.PORT || 5050;
 
 app.use(express.json());
+
+app.use("/api/warehouses", warehouseRoutes);
 
 app.listen(PORT, () => {
   console.log(`running at http://localhost:${PORT}`);
