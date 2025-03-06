283971cbb6476445cb6a2f1942818340464d0580
NicoleBiii <xinyuebi23@gmail.com>
2025-03-05 22:35:31 -0500

    add getInventories in inventory routes and controller

--- controllers/inventory-controller.js
+++ controllers/inventory-controller.js
@@ -29,7 +29,12 @@ const addInventory = async (req, res) => {
 };
 
 const getInventories = async (req, res) => {
-    
+    try {
+      const data = await knex("inventories");
+      res.status(200).json(data);
+    } catch (error) {
+      res.status(400).send(`Error retrieving inventories: ${error}`);
+    }
 }
 
-export { addInventory };
+export { addInventory, getInventories };
